de2652eae29eb1c119e84ecf96e41f13b413f971
annruiblaz <anruiblaz@gmail.com>
2025-02-02 23:40:12 +0100

    style fixes + solicita permiso

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ if (getUserMediaSupported()) {
     console.warn('getUserMedia no es compatible con el navegador');
 }
 
+var model = true;
+
 //  Activa la webcam y comienza a buscar objetos(?)
 function enableCam(event) {
     //  Sale de la función si no está cargado COCO-SSD
@@ -35,67 +37,10 @@ function enableCam(event) {
             video.srcObject = stream;
             video.addEventListener('loadeddata', predictWebcam);//  Necesario para evitar "bugs" xq el asignar el stream tarda un tiempo
         });
-
-    function predictWebcam() {
-
-    }
-
-
-    // Store the resulting model in the global scope of our app.
-    var model = undefined;
-
-    // Before we can use COCO-SSD class we must wait for it to finish
-    // loading. Machine Learning models can be large and take a moment 
-    // to get everything needed to run.
-    // Note: cocoSsd is an external object loaded from our index.html
-    // script tag import so ignore any warning in Glitch.
-    cocoSsd.load().then(function (loadedModel) {
-    model = loadedModel;
-    // Show demo section now model is ready to use.
-    demosSection.classList.remove('invisible');
-    });
-
-    var children = [];
-
-    function predictWebcam() {
-    // Now let's start classifying a frame in the stream.
-    model.detect(video).then(function (predictions) {
-        // Remove any highlighting we did previous frame.
-        for (let i = 0; i < children.length; i++) {
-        liveView.removeChild(children[i]);
-        }
-        children.splice(0);
-        
-        // Now lets loop through predictions and draw them to the live view if
-        // they have a high confidence score.
-        for (let n = 0; n < predictions.length; n++) {
-        // If we are over 66% sure we are sure we classified it right, draw it!
-        if (predictions[n].score > 0.66) {
-            const p = document.createElement('p');
-            p.innerText = predictions[n].class  + ' - with ' 
-                + Math.round(parseFloat(predictions[n].score) * 100) 
-                + '% confidence.';
-            p.style = 'margin-left: ' + predictions[n].bbox[0] + 'px; margin-top: '
-                + (predictions[n].bbox[1] - 10) + 'px; width: ' 
-                + (predictions[n].bbox[2] - 10) + 'px; top: 0; left: 0;';
-
-            const highlighter = document.createElement('div');
-            highlighter.setAttribute('class', 'highlighter');
-            highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: '
-                + predictions[n].bbox[1] + 'px; width: ' 
-                + predictions[n].bbox[2] + 'px; height: '
-                + predictions[n].bbox[3] + 'px;';
-
-            liveView.appendChild(highlighter);
-            liveView.appendChild(p);
-            children.push(highlighter);
-            children.push(p);
-        }
-        }
-        
-        // Call this function again to keep predicting when the browser is ready.
-        window.requestAnimationFrame(predictWebcam);
-    });
-    }
-
 }
+
+// Función de placeholder
+function predictWebcam() {}
+
+// Hace visible la sección de demostración
+demosSection.classList.remove('invisible');
